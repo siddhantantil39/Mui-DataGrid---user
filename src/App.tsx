1d03ad14eb46090958046619556e0abac3e3a9ec
siddhantantil39 <siddhantantil39@gmail.com>
2025-01-21 01:58:10 +0530

    Added fallback after lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { lazy } from 'react';
+import { lazy, Suspense } from 'react';
 import './App.css'
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { CircularProgress } from '@mui/material';
 
 const Details = lazy(() => import('./components/Details/Details'));
 const Grid = lazy(() => import('./components/Grid/Grid'));
@@ -9,13 +10,15 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Grid/>}/>
-        <Route path='/details/:id' element={<Details/>}/>
-        <Route path="/details" element={<Navigate to="/" replace />} />
-      </Routes>
+      <Suspense fallback={<CircularProgress/>}>
+        <Routes>
+          <Route path="/" element={<Grid />} />
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/details" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App
